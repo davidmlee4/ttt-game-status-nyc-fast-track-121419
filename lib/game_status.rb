d6f3161bdc8e5286d26d643364a03e86538cdcdd
benevolent-trace-0172 <davidmlee4@gmail.com>
2019-12-15 19:22:56 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -37,12 +37,7 @@ def won?(board)
     end
 end
 
-def check_win_combination?(board, player, win_combo)
-  win_combo.all? do |position|
-    board[position] == player
-  end
-end
-
+\
 def full?(board)
   if board.include? ('') || board.include? (' ')
     return false
